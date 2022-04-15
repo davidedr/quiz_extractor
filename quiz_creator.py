60a4955c85192f5f16d7dac9f3e80ce87d370b60
DavideDR <d.dr@libero.it>
2022-04-16 00:58:26 +0200

    Links to create_quiz

--- quiz_creator.py
+++ quiz_creator.py
@@ -4,7 +4,7 @@ import argparse
 import logging
 import random
 
-from utility import read_db_config, connect_test
+from utility import read_db_config, connect_test, create_quiz
 
 format = "%(asctime)s:%(thread)d:%(threadName)s:%(levelname)s:%(message)s"
 logging.basicConfig(filename = "log/quiz_creator.log", format = format, level = logging.NOTSET, datefmt = "%Y-%m-%d %H:%M:%S")
@@ -33,7 +33,8 @@ db_test_ok = connect_test(db_config)
 if not db_test_ok:
   end
 
-quiz = []
+broken = False
+chosen_ids = []
 for quiz_number in quiz_numbers:
   topic = quiz_number["topic"]
   how_many = quiz_number["how_many"]
@@ -68,6 +69,90 @@ for quiz_number in quiz_numbers:
         connection.close()
         logging.debug('Database connection closed.')
 
-  chosen = random.sample(ids, how_many)
-  logging.error(f'topic: {topic}, how_many: {how_many}, ids: "{ids}", len: {len(ids)}')
-  logging.error(f'chosen: "{chosen}", len: {len(chosen)}')
+  chosen = []
+  try: 
+    chosen = random.sample(ids, how_many)
+  except (Exception) as e:
+    logging.error(f'Exception in random: {e}')
+    logging.error(f'ids: "{ids}" (len: {len(ids)}), how_many: {how_many}!')
+    broken = True
+    break
+
+  logging.info(f'topic: {topic}, how_many: {how_many}, ids: "{ids}", len: {len(ids)}')
+  logging.info(f'chosen: "{chosen}", len: {len(chosen)}')
+  chosen_ids.append(chosen)
+
+print(broken, chosen_ids)
+if broken:
+  logging.error(f'Broken "{broken}"!')
+  end
+
+quiz_items = []
+for chosen_id in chosen_ids:
+  for chosen in chosen_id:
+    row_question = None
+    rows_answers = None
+    try:
+      connection = psycopg2.connect(**db_config)
+      cursor = connection.cursor()
+      query = "SELECT * FROM questions WHERE id=%s"
+      values = (chosen,)
+      logging.error(f'query: "{query}"!')
+      logging.error(f'values: "{values}"!')
+      cursor.execute(query, values)
+      row_question = cursor.fetchone()
+      cursor.close()
+
+      cursor = connection.cursor()
+      query = "SELECT * FROM answers WHERE question_id=%s ORDER BY answer_no"
+      values = (chosen,)
+      logging.error(f'query: "{query}"!')
+      logging.error(f'values: "{values}"!')
+      cursor.execute(query, values)
+      rows_answers = cursor.fetchmany(3)
+      cursor.close()
+
+    except (Exception, psycopg2.DatabaseError) as e:
+        logging.error(f'Exception "{e}"!')
+        break
+    finally:
+        if connection is not None:
+          connection.close()
+          logging.debug('Database connection closed.')
+
+    if not row_question or not rows_answers or len(rows_answers)<3:
+      break
+    
+    question_id = row_question[0]
+    question_number = row_question[1]
+    question_subject = row_question[2]
+    question_version = row_question[3]
+    question_theme = row_question[4]
+    question_topic = row_question[5]
+    question_section = row_question[6]
+    question_question = row_question[7]
+    question_image = row_question[8]
+    question_image_filename = row_question[9]
+    question_image_width = row_question[10]
+    question_image_height = row_question[11]
+
+    answers = []
+    for rows_answer in rows_answers:
+      answer_id = rows_answer[0]
+      answer_question_id = rows_answer[1]
+      answer_no = rows_answer[2]
+      answer_answer = rows_answer[3]
+      answer_correct = rows_answer[4]
+      answer = {"answer_no": answer_no, "answer": answer_answer, "correct": answer_correct}
+      answers.append(answer)
+
+    quiz_item = {
+      "question_no": question_number,
+      "question": question_question,
+      "image": {"image_binary": question_image, "image_width": question_image_width, "image_height": question_image_height},
+      "answers": answers
+    }
+
+    quiz_items.append(quiz_item)
+
+create_quiz(quiz_items)
--- utility.py
+++ utility.py
@@ -3,37 +3,113 @@ import psycopg2
 import logging
 
 def read_db_config(filename = 'database.ini', section = 'quiz_postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
+  parser = ConfigParser()
+  parser.read(filename)
 
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        e = Exception('Section {0} not found in the {1} file'.format(section, filename))
-        logging.info(f'{e}')
-        raise e
+  db = {}
+  if parser.has_section(section):
+      params = parser.items(section)
+      for param in params:
+          db[param[0]] = param[1]
+  else:
+      e = Exception('Section {0} not found in the {1} file'.format(section, filename))
+      logging.info(f'{e}')
+      raise e
 
-    return db
+  return db
 
 def connect_test(db_config):
-    connection = None
-    try:
-        logging.info('Connecting to the PostgreSQL database...')
-        connection = psycopg2.connect(**db_config)
-        cursor = connection.cursor()
-        cursor.execute('SELECT version()')
-        db_version = cursor.fetchone()
-        logging.info(f'PostgreSQL database version: "{db_version}"!')
-        cursor.close()
-    except (Exception, psycopg2.DatabaseError) as e:
-        logging.error(f'Exceptinon testing database: "{e}"!')
-        return False
-    finally:
-        if connection is not None:
-          connection.close()
-          logging.debug('Database connection closed.')
+  connection = None
+  try:
+      logging.info('Connecting to the PostgreSQL database...')
+      connection = psycopg2.connect(**db_config)
+      cursor = connection.cursor()
+      cursor.execute('SELECT version()')
+      db_version = cursor.fetchone()
+      logging.info(f'PostgreSQL database version: "{db_version}"!')
+      cursor.close()
+  except (Exception, psycopg2.DatabaseError) as e:
+      logging.error(f'Exceptinon testing database: "{e}"!')
+      return False
+  finally:
+      if connection is not None:
+        connection.close()
+        logging.debug('Database connection closed.')
 
-    return True
+  return True
+
+QUIZ_SUBFOLDER = "quiz"
+
+def create_quiz(quiz_items):
+  html_out_header = '''<!DOCTYPE html>
+  <html>
+
+  <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <style>
+          * {
+              box-sizing: border-box;
+          }
+          
+          .row {
+              display: flex;
+          }
+          /* Create three equal columns that sits next to each other */
+          
+          .column1 {
+              flex: 80%;
+              padding: 5px;
+          }
+          
+          .column2 {
+              flex: 20%;
+              padding: 5px;
+          }
+      </style>
+  </head>
+
+  <body>
+
+      <h2>Text and image side by side</h2>
+      <p>How to put text and image side by side with CSS Flexbox:</p>
+  '''
+
+  html_out = html_out_header
+  for quiz_item in quiz_items:
+    html_out = html_out + f"""    <div class="row">
+            <div class="column1">
+                <p><b>{quiz_item["question_no"]}</b> {quiz_item["question"]}</p>
+                <ul>
+                  <li style="list-style-type:none">
+                      <dl>
+                          <dt><input type="checkbox">  {quiz_item["answers"][0]["answer"]}</dt>
+                      </dl>
+                  </li>
+                  <li style="list-style-type:none">
+                      <dl>
+                          <dt><input type="checkbox">  {quiz_item["answers"][1]["answer"]}</dt>
+                      </dl>
+                  </li>
+                  <li style="list-style-type:none">
+                      <dl>
+                          <dt><input type="checkbox">  {quiz_item["answers"][2]["answer"]}</dt>
+                      </dl>
+                  </li>
+
+                </ul>
+            </div>
+            <div class="column2">
+                <img src="images/13.jpeg" alt="" width="150" height="130">
+            </div>
+        </div>"""
+
+  html_out_trailer = """</body>
+
+  </html>"""
+
+  quiz_number = 1
+  html_out = html_out + html_out_trailer
+  filename = f'{QUIZ_SUBFOLDER}/{str(quiz_number)}.html'
+  html_out_file = open(filename, 'w')
+  html_out_file.write(html_out)
+  html_out_file.close()
