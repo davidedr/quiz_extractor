138bd417fcdfd0a7df09544983c74c4ff5ae0202
DavideDR <d.dr@libero.it>
2022-04-15 23:25:08 +0200

    Normalizes db

--- quiz_creator.py
+++ quiz_creator.py
@@ -3,9 +3,12 @@ import argparse
 
 import logging
 
+from utility import read_db_config, connect_test
+
 logging.basicConfig(filename = "log/quiz_creator.log", format = format, level = logging.NOTSET, datefmt = "%Y-%m-%d %H:%M:%S")
 
 argsparser = argparse.ArgumentParser(prog = "quiz_creator", usage = '%(prog)s [options] session', description = 'Generate a quiz for the given session')
 argsparser.add_argument('Session', metavar = 'session', type = int, help = 'the session to be used')
 args = argsparser.parse_args()
 session = args.session
+
